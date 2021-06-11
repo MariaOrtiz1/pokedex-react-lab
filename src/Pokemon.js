821a1bd8d898a49e1063f7d4331be09a71a4a3a9
Maria Ortiz-Lopez <OrtizLopez.Maria030@gmail.com>
2021-06-10 22:10:50 -0700

    It works, yeah! Time to add css later!

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import request from 'superagent';
 import Spinner from './Spinner';
+import Sort from './Sort';
 
 const sleep = (x) => new Promise((res, rej) => setTimeout(() => { res() }, x))
 
@@ -10,6 +11,7 @@ export default class Pokemon extends Component {
         pokemonName: [],
         loading: false,
         query: '',
+        sortOrder: '',
     }
 
     componentDidMount = async () => {
@@ -27,30 +29,38 @@ export default class Pokemon extends Component {
     fetchData = async () => {
         this.setState({ loading: true });
 
+
         const URL = this.state.query
-        ? `https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.query}`
-        : 'https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon';
+        ? `https://pokedex-alchemy.herokuapp.com/api/pokedex?pokemon=${this.state.query}&sort=pokemon&direction=${this.state.sortOrder}`
+        : `https://pokedex-alchemy.herokuapp.com/api/pokedex?sort=pokemon&direction=${this.state.sortOrder}`;
 
         const data = await request.get(URL)
-        await sleep(3000)
+        await sleep(1000)
 
         this.setState({ loading: false });
-        this.setState({ pokemonName: data.body.results });
+        this.setState({ pokemonName: data.body.results, sortOrder: data.body.sort });
 
+    }
+
+    handleSort = async (e) => {
+        this.setState({sortOrder: e.target.value})
     }
 
     render() {
         return (
             <div className="center">
                 <input onChange={this.handleChange} />
-                <button onClick={this.handleClick}>Fetch!</button>
+                <button onClick={this.handleClick}> Fetch! </button>
+                <Sort event={this.handleSort}/>
                 {this.state.loading
                 ? <Spinner />
                 : this.state.pokemonName.map(pokemon =>
                 <p>
                     {pokemon.pokemon}
                     <br></br>
-                    <img width="30px" src={pokemon.url_image} alt={pokemon.pokemon} />
+                    <img width="100px" height="100px" src={pokemon.url_image} alt={pokemon.pokemon} />
+                    <br></br>
+                    Type:
                     <br></br>
                     {pokemon.type_1}
                     <br></br>
